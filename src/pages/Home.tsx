c04b706d02292a775aadce3dec73c009f1488fee
Faqih Muntashir <itsfaqih@gmail.com>
2020-04-03 08:37:43 +0700

    Added the project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,63 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonInput, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
+import React, { useState } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [scorePlayer1, setScorePlayer1] = useState(0);
+  const [scorePlayer2, setScorePlayer2] = useState(0);
+
+  function addScorePlayer1() {
+    setScorePlayer1(score => (scorePlayer1 + 1));
+  }
+
+  function delScorePlayer1() {
+    setScorePlayer1(score => scorePlayer1 - 1);
+  }
+
+  function addScorePlayer2() {
+    setScorePlayer2(score => (scorePlayer2 + 1));
+  }
+
+  function delScorePlayer2() {
+    setScorePlayer2(score => scorePlayer2 - 1);
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Badminton Scoring</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <div>Albert</div>
+              <div>
+                <IonButton onClick={addScorePlayer1}>+</IonButton>
+              </div>
+              <div>
+                <IonInput value={String(scorePlayer1)}></IonInput>
+              </div>
+              <div>
+                <IonButton onClick={delScorePlayer1}>-</IonButton>
+              </div>
+            </IonCol>
+            <IonCol>
+              <div>Einstein</div>
+              <div>
+                <IonButton onClick={addScorePlayer2}>+</IonButton>
+              </div>
+              <div>
+                <IonInput value={String(scorePlayer2)}></IonInput>
+              </div>
+              <div>
+                <IonButton onClick={delScorePlayer2}>-</IonButton>
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
